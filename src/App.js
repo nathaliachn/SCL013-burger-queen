51b1bb64ba2d0bf61b1fcfcba5cf1a8272b136f2
eilianacar <eilianacar@gmail.com>
2020-07-07 13:19:29 -0400

    fusionando rama con nathalia

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import { useFirebaseApp } from 'reactfire';
-=======
 import './App.css';
 import Auth  from './Auth';
 
->>>>>>> dd1cb182eca3ab69837d6aeb35f5a6d7110348ff
 function App() {
   const firebase = useFirebaseApp();
   
   console.log(firebase);
   return (
-<<<<<<< HEAD
-       <div className='container'>
-      <h1>NavBar</h1>
-      <hr />
-      
-     </div>
-    
-=======
     <div className="App">
       <p>usuario:</p>
       <Auth/>
     </div>
->>>>>>> dd1cb182eca3ab69837d6aeb35f5a6d7110348ff
   );
 }
 
