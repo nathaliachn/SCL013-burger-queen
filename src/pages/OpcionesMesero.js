c637641a597a661684d309832a8d836d9bac1487
eilianacar <eilianacar@gmail.com>
2020-07-14 09:27:11 -0400

    Funcion Eliminar

--- src/pages/OpcionesMesero.js
+++ src/pages/OpcionesMesero.js
@@ -5,11 +5,18 @@ import data from "../data.json";
 export default function Opciones() {
   const [menuType, setMenuType] = useState("Sweets");
   const [selectedItems, setSelectedItems] = useState([]);
+
   const handleItemClick = item => {
     // Incluye la logica para saber cuando el elemento
     // ya existe en el arreglo.
     setSelectedItems([...selectedItems, item]);
   };
+
+  const handleDeleteItem = deletedId => {
+    const fiteredItems = selectedItems.filter(({ id }) => id !== deletedId)
+
+    setSelectedItems(fiteredItems)
+  }
 
   return (
     <div className="App">
@@ -19,14 +26,49 @@ export default function Opciones() {
             {item}
           </button>
         ))}
-        <br/>
-        <br/>
+        <br />
+        <br />
         {data[menuType].map(item => (
           <div className="App-menu__item" onClick={() => handleItemClick(item)}>
-            {item.producto} <span>${item.precio}</span> 
+            {item.producto} <span>${item.precio}</span>
           </div>
         ))}
-      </div>     
+      </div>
+      <div className="App-list">
+        <div className="row">
+          <div className="col-sm">
+            <h1><i class="fas fa-concierge-bell"></i>Pedidos</h1>
+
+
+
+            {selectedItems &&
+              selectedItems.map(item => (
+                <div className="App-list__item">
+                  {item.producto} <span>${item.precio}</span>
+                  <button onClick={() => handleDeleteItem(item.id)}>Eliminar</button>
+                </div>
+              ))}
+            <div className="App-list__item App-list__total">
+              Total{" "}
+              <span>
+                $
+           {selectedItems
+                  .reduce((acc, curr) => acc + curr.precio, 0)
+                  .toFixed(2)}
+              </span>
+            </div>
+            {selectedItems.length > 0 && (
+              <button
+                className="App-list__button"
+                onClick={() => alert("enviando a cocina")}
+              >
+                Enviar a cocina
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
