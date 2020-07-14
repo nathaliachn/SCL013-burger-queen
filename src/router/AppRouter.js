ff8fac6c0de6daf057a2e9349153866ed00cfd64
eilianacar <eilianacar@gmail.com>
2020-07-14 10:42:36 -0400

    eliminando div que no sirven

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router,Switch,Route,Link, } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
 import { ContactPage } from '../pages/ContactPage';
 import { SelectionPage } from '../pages/SelectionPage';
@@ -10,26 +10,24 @@ import { VistasCocinero } from '../pages/VistaCocinero';
 
 export const AppRouter = () => {
   return (
-      <Router>
-      <div className ='container-mt-5'>
-        <div className ='btn-group'>
-           <Link to="/register" className='btn btn-dark'>Registrate</Link>
-           <Link to="/login" className='btn btn-dark'>Inicia Sesión</Link>
-            <Link to="/contact" className='btn btn-dark'>Nosotros</Link>
-        </div>
-
-        <Switch>
-          
-          <Route path="/contact" component= { ContactPage } />
-          <Route path="/selection" component= { SelectionPage } />
-          <Route path="/register" component= { Registro } />
-          <Route path="/login" component= { Login } />
-          <Route path="/mesero" component= { VistasMesero } />
-          <Route path="/cocinero" component= { VistasCocinero } />
-          <Route path="/" component= { HomePage } />
-                            
-        </Switch>
+    <Router>
+      <div className='btn-group'>
+        <Link to="/register" className='btn btn-dark'>Registrate</Link>
+        <Link to="/login" className='btn btn-dark'>Inicia Sesión</Link>
+        <Link to="/contact" className='btn btn-dark'>Nosotros</Link>
       </div>
+      <Switch>
+
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/selection" component={SelectionPage} />
+        <Route path="/register" component={Registro} />
+        <Route path="/login" component={Login} />
+        <Route path="/mesero" component={VistasMesero} />
+        <Route path="/cocinero" component={VistasCocinero} />
+        <Route path="/" component={HomePage} />
+
+      </Switch>
+
     </Router>
   );
 }
