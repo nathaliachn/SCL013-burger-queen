c27ac7a82480ebc7c0afd0e997c33b136726fd31
eilianacar <eilianacar@gmail.com>
2020-07-19 14:26:33 -0400

    imagenes agregadas en homepage y selectionpage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,15 +2,19 @@ import React from 'react';
 import '../img/Inicio.css';
 import { Link } from 'react-router-dom';
 import { Fragment } from 'react';
+import theCakeHouse from '../img/theCakeHouse.png'
 
 export const HomePage = () => {
   return (
     <Fragment>
       <div id='startBackground'>
+        <div className="imagenFondo">
+          <img className="fondo" alt="" src={theCakeHouse} />
+        </div>
         <h1 id='startTittle'>
           The cake <br></br>house</h1>
-        <Link to='/selection'>
-          <button id ='startButton'>Comenzar</button>
+        <Link to='/login'>
+          <button id='startButton'>Comenzar</button>
         </Link>
       </div>
     </Fragment >
--- src/pages/SelectionPage.js
+++ src/pages/SelectionPage.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import '../img/Inicio.css';
+import imagerGarzon from '../img/imagerGarzon.jpg'
+import imagenPastelera from '../img/imagenPastelera.jpg'
 
 export const SelectionPage = () => {
-return (
-    <div id= 'selection'>
-          <section id='waiterSection'>
-               <Link to= '/login'>
-               <div id='waiterPicture'></div>
-               <button id='waiterButton'type="button" title='Mesero' class="btn btn-primary btn-lg btn-block">Mesero</button>
-               </Link>
-          </section>
-          <section id='chefSection'>
-               <img src="../fotopastelero.png" alt="" id="waiterpicture"></img>
-               <Link to= '/login'>
-               <button  id='chefButton' type="button" title='Cocina' class="btn btn-primary btn-lg btn-block">Cocina</button>
-               </Link>
-          </section>
-    </div>
+     return (
+          <div id='selection'>
+               <section id='waiterSection'>
+                    <Link to='/mesero'>
+                         <img className="imagenSelection" alt="" src={imagerGarzon} />
+                         <button id='waiterButton' type="button" title='Mesero' class="btn btn-primary btn-lg btn-block">Mesero</button>
+                    </Link>
+               </section>
+               <section id='chefSection'>
+                    <img className="imagenSelection" alt="" src={imagenPastelera} />
+                    <Link to='/cocinero'>
+                         <button id='chefButton' type="button" title='Cocina' class="btn btn-primary btn-lg btn-block">Cocina</button>
+                    </Link>
+               </section>
+          </div>
      )
 }
