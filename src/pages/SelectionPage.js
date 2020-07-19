cf8a2a536bc9c17f99affa6f9f59c1cd4aa982a4
nathaliachn <nathaliachn@gmail.com>
2020-07-18 22:43:50 -0400

    se aceptaron cambios de la rama cronometro y se ajustaron todos los cambios para subirse a la rama master

--- src/pages/SelectionPage.js
+++ src/pages/SelectionPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import '../img/Inicio.css';
 
 export const SelectionPage = () => {
@@ -9,35 +8,15 @@ return (
           <section id='waiterSection'>
                <Link to= '/login'>
                <div id='waiterPicture'></div>
-               <button id='waiterButton'type="button" title='Mesero' class="btn btn-primary btn-lg btn-block">Mesero</button>
+               <button id='waiterButton'type="button">Mesero</button>
                </Link>
           </section>
           <section id='chefSection'>
                <img src="../fotopastelero.png" alt="" id="waiterpicture"></img>
                <Link to= '/login'>
-               <button  id='chefButton' type="button" title='Cocina' class="btn btn-primary btn-lg btn-block">Cocina</button>
+               <button  id='chefButton' type="button">Cocina</button>
                </Link>
           </section>
     </div>
      )
-=======
-
-export const SelectionPage = () => {
-    return (
-        <div id='selection'>
-            <div>
-                <section>
-                    <Link to='/mesero'>
-                        <button>Mesero</button>
-                    </Link>
-                </section>
-                <section>
-                    <Link to='/cocinero'>
-                        <button>Cocinero</button>
-                    </Link>
-                </section>
-            </div>
-        </div>
-    );
->>>>>>> 24832c9300bf45ffc471f30fb8e1cfd514d345e8
 }
